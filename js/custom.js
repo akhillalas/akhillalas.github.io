3a31e01e8b7c6d82a50ca150e1790a4fa3b76e31
akhillalas <113799423+akhillalas@users.noreply.github.com>
2022-09-18 12:57:16 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+// templatemo 467 easy profile
+
+// PRELOADER
+
+$(window).load(function(){
+    $('.preloader').delay(1000).fadeOut("slow"); // set duration in brackets    
+});
+
+// HOME BACKGROUND SLIDESHOW
+$(function(){
+    jQuery(document).ready(function() {
+		$('body').backstretch([
+	 		 "images/1.jpg",
+			  "images/2.jpg",
+			   "images/3.jpg",
+			    "images/4.jpg",
+	 			], 	{duration: 6300, fade: 1300});
+		});
+})
